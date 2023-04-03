fa50d81896189d4b67c7c9715c1a233058dfa4d2
LucasBrasileiroRaposo <lucas.raposo@ccc.ufcg.edu.br>
2023-04-03 18:19:10 -0300

    refact code to read all questions and map to respectives lists

--- testConvert.py
+++ testConvert.py
@@ -1,7 +1,7 @@
 import PyPDF2
 
 # Abrir o arquivo PDF em modo de leitura binária
-with open('2022_PV_digital_D1_CD1_ingles.pdf', 'rb') as arquivo_pdf:
+with open('pdfs/2022_PV_digital_D1_CD1_ingles.pdf', 'rb') as arquivo_pdf:
 
     # Criar objeto leitor do PyPDF2
     leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
@@ -11,25 +11,38 @@ with open('2022_PV_digital_D1_CD1_ingles.pdf', 'rb') as arquivo_pdf:
 
     questoesHumanas = []
     questoesLinguagens = []
-    contador = 90
+    redacao = []
+    identificador = 1
+    contador = 0
 
     # Loop por todas as páginas do PDF
     for num_pagina in range(len(leitor_pdf.pages)):
         pagina = leitor_pdf.pages[num_pagina]
 
-        # contador += 1
-        aux2 = ""
 
         if "Questão" in pagina.extract_text():
-            if contador <= 45:
-                questoesLinguagens.append(pagina.extract_text().split("Questão %2d - Linguagens, Códigos e suas Tecnologias", contador))
-                contador -= 1
-            elif contador > 45:
-                questoesHumanas.append(pagina.extract_text().split("Questão %2d - Ciências Humanas e suas Tecnologias", contador))
-                contador -= 1
-            # questoesHumanas.append(pagina.extract_text().split("Questão 01 - Linguagens, Código e suas Tecnologias"))
+            if identificador <= 45:
+                questoesLinguagens.append([identificador,pagina.extract_text().split("Questão %2d - Linguagens, Códigos e suas Tecnologias", identificador)])
+                identificador += 1
+                contador += 1
+            elif identificador > 45:
+                contador = 0
+                questoesHumanas.append([identificador,pagina.extract_text().split("Questão %2d - Ciências Humanas e suas Tecnologias", identificador)])
+                identificador += 1
+                contador += 1
+        else:
+            if identificador == 46:
+                redacao.append(pagina.extract_text())
+            elif  contador <= len(questoesLinguagens) and contador > 0 and identificador <= 45:
+                questoesLinguagens[contador-1][1].append(pagina.extract_text())
+            elif contador > 0 and contador <= len(questoesHumanas) and identificador > 45:
+                questoesHumanas[contador-1][1].append(pagina.extract_text())              
+
+
         # Extrair o texto da página
     
 # Imprimir o texto extraído
-print("{} \n {}".format(questoesHumanas,questoesLinguagens))
-print(contador)
+# print("{} \n {}".format(questoesHumanas,questoesLinguagens))
+print(redacao)
+print(questoesLinguagens)
+# print(questoesHumanas)
