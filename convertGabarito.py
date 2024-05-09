5ecf3fe417b3d00b8e80278fc7e8ac30675c7229
LucasBrasileiroRaposo <lucas.raposo@ccc.ufcg.edu.br>
2024-05-08 23:23:02 -0300

    delete unused files

--- convertGabarito.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-with open('pdfs/2022_GB_impresso_D1_CD1.pdf', 'rb') as arquivo_pdf:
-
-    leitor_pdf = PyPDF2.PdfReader(arquivo_pdf)
-
-    pagina = leitor_pdf.pages[0]
-
-    print(pagina.extract_text())
